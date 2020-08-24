f97c85ba9742493698aee238d0b9e20fa41081b5
Matthew Everett <matthew@mjheverett.com>
2020-08-24 17:07:25 -0400

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,27 @@
 'use strict'
 
-const chuckSays = document.getElementById('chuckSays');
-const refreshButton = document.getElementById('refreshQuote');
+const weatherDisplay = document.getElementById('weatherDisplay');
+const submitFormButton = document.getElementById('submitForm');
+const defaultCity = "Atlanta";
 
-refreshButton.addEventListener('click', function () {
-    const url = "https://api.chucknorris.io/jokes/random?category=dev";
+console.log(submitFormButton)
+
+const getWeather = (city) => {
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c0ae55bcbeaae633fae083cbaa9bdbfb&units=imperial`;
     get(url).then(function(fetchResponse) {
-        chuckSays.innerHTML = fetchResponse.value;
-    })
-})
+        weatherDisplay.innerHTML = `Current Temp: ${fetchResponse.main.temp} F`;
+        console.log(fetchResponse);
+    });
+}
+
+submitFormButton.addEventListener('click', function (e) {
+    e.preventDefault();
+    const cityInput = document.getElementById('locationInput');
+    console.log("user city", cityInput)
+    const locationName = cityInput.value;
+    getWeather(locationName);
+});
+
+(function () {
+    getWeather(defaultCity);
+})();
