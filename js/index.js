fb3f870e68997ff3b4b3a3fac314aa6f11d1a73d
Matthew Everett <matthew@mjheverett.com>
2020-08-24 14:57:30 -0400

    connected button

--- js/index.js
+++ js/index.js
@@ -3,21 +3,9 @@
 const chuckSays = document.getElementById('chuckSays');
 const refreshButton = document.getElementById('refreshQuote');
 
-function get() {
-    //Step 1: fetch the data
-    return fetch("https://api.chucknorris.io/jokes/random?category=dev")
-    //Step 2: run the json() method from the response
-        .then(function(response) {
-            return response.json();
-        })
-    //Step 3: Return the data from the response.json() method
-        .then(function(data) {
-            return data;
-        })
-}
-
-(function() {
-    get().then(function(fetchResponse) {
+refreshButton.addEventListener('click', function () {
+    const url = "https://api.chucknorris.io/jokes/random?category=dev";
+    get(url).then(function(fetchResponse) {
         chuckSays.innerHTML = fetchResponse.value;
     })
-})();
+})
