26745ed778c1f6e2e83d1b9395028c062731cc91
Matthew Everett <matthew@mjheverett.com>
2020-08-24 12:55:13 -0400

    connected api to html to use and IIFE to generate quite

--- js/index.js
+++ js/index.js
@@ -1,2 +1,23 @@
+'use strict'
+
+const chuckSays = document.getElementById('chuckSays');
 const refreshButton = document.getElementById('refreshQuote');
-const quoteBox = document.getElementById('chuckSays')
+
+function get() {
+    //Step 1: fetch the data
+    return fetch("https://api.chucknorris.io/jokes/random?category=dev")
+    //Step 2: run the json() method from the response
+        .then(function(response) {
+            return response.json();
+        })
+    //Step 3: Return the data from the response.json() method
+        .then(function(data) {
+            return data;
+        })
+}
+
+(function() {
+    get().then(function(fetchResponse) {
+        chuckSays.innerHTML = fetchResponse.value;
+    })
+})();
