69eaf83f4d893443f2d359eabf4f31bdfac298e8
Matthew Everett <matthew@mjheverett.com>
2020-08-25 14:15:44 -0400

    added additional weather variables

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
 'use strict'
 
-const weatherDisplay = document.getElementById('weatherDisplay');
 const submitFormButton = document.getElementById('submitForm');
 const defaultCity = "Atlanta";
 
 console.log(submitFormButton)
 
 const getWeather = (city) => {
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c0ae55bcbeaae633fae083cbaa9bdbfb&units=imperial`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c0ae55bcbeaae633fae083cbaa9bdbfb&units=imperial`;
     get(url).then(function(fetchResponse) {
-        weatherDisplay.innerHTML = `Current Temp: ${fetchResponse.main.temp} F`;
-        console.log(fetchResponse);
+        const cityName = document.getElementById('cityName');
+        cityName.innerHTML = fetchResponse.name;
+        const currentTemp = document.getElementById('currentTemp');
+        currentTemp.innerHTML = `Temperature: ${fetchResponse.main.temp} F`;
+        const feelsLikeTemp = document.getElementById('feelsLikeTemp');
+        feelsLikeTemp.innerHTML = `Feels Like: ${fetchResponse.main.feels_like} F`;
+        const currentHumid = document.getElementById('currentHumid');
+        currentHumid.innerHTML = `Humidity: ${fetchResponse.main.humidity} %`;
+        const currentWind = document.getElementById('currentWind');
+        currentWind.innerHTML = `Wind Speed: ${fetchResponse.wind.speed} mph`;
     });
 }
 
